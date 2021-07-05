01da38c6776ae459ceb6f3f9b750951a00451500
Fani Abdullah <faniabdullah27@gmail.com>
2021-07-05 20:48:34 +0700

    insertionSort

---
+++ src/algorithms/sorting/InsertionSort.kt
@@ -0,0 +1,29 @@
+package algorithms.sorting
+
+fun insertionSort(array: ArrayList<Int>){
+
+    for (i in array.indices){
+            if (array[i] <= array[0]){
+              array.add(0,array.removeAt(i))
+            } else {
+                if (array[i] < array[i-1]){
+                    for (j in 1 until i){
+                        if (array[i] < array[j]){
+                            array.add(j, array.removeAt(i))
+                            break
+                        }
+                    }
+                }
+            }
+    }
+}
+
+
+
+
+fun main(){
+    val numbers = ArrayList(listOf(99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0))
+    insertionSort(numbers)
+    println(numbers)
+
+}
