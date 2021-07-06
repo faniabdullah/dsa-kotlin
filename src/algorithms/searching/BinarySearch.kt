4c9c60863e1857d1bc3447086d21083af46ab3c3
Fani Abdullah <faniabdullah27@gmail.com>
2021-07-06 22:12:41 +0700

    Binary Search

---
+++ src/algorithms/searching/BinarySearch.kt
@@ -0,0 +1,33 @@
+package algorithms.searching
+
+class BinarySearch {
+    fun binarySearch(arr: IntArray, key: Int): String {
+        var first = 0
+        var last = arr.size - 1
+        var middle = (first + last) / 2
+        while (first <= last) {
+            when {
+                arr[middle] < key -> {
+                    first = middle + 1
+                }
+                arr[middle] == key -> {
+                    return "Element Found At Index $middle"
+                }
+                else -> {
+                    last = middle - 1
+                }
+            }
+            middle = (first + last) / 2
+        }
+        return "Element Not Found"
+    }
+}
+
+fun main() {
+    val binarySearch = BinarySearch()
+    val array = intArrayOf(10, 25, 32, 45, 55, 68 , 150)
+    val result = binarySearch.binarySearch(array, 55)
+    println(result)
+    val result2 = binarySearch.binarySearch(array, 100)
+    println(result2)
+}
