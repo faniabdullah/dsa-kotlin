72410df8eae0dfa725657e73994ee99602e601d2
Fani Abdullah <faniabdullah27@gmail.com>
2021-07-20 22:36:04 +0700

    queue using stack

--- src/data_structure/queue/QueueUsingStack.kt
+++ src/data_structure/queue/QueueUsingStack.kt
@@ -1,7 +1,59 @@
 package data_structure.queue
 
+import java.util.*
+
+
+class QueueUsingStack {
+    var queueStack = Stack<Int>()
+
+    private var revStack = Stack<Int>()
+
+    fun push(x: Int) {
+        if (queueStack.size == 0) {
+            queueStack.push(x)
+        } else {
+            while (queueStack.size != 0) {
+                revStack.push(queueStack.pop())
+            }
+            revStack.push(x)
+
+            while (revStack.size != 0) {
+                queueStack.push(revStack.pop())
+            }
+        }
+    }
+
+    fun pop(): Int? {
+        return if (queueStack.size > 0) {
+            queueStack.pop()
+        } else {
+            null
+        }
+    }
+
+
+    fun peek(): Int? {
+        return if (queueStack.size > 0) {
+            queueStack[queueStack.size - 1]
+        } else {
+            null
+        }
+    }
+
+    fun empty() = queueStack.size == 0
+
+}
 
 
 fun main() {
-
+    val myQueue = QueueUsingStack()
+    myQueue.push(10)
+    myQueue.push(20)
+    myQueue.push(30)
+    println("queue: " + myQueue.queueStack)
+    println("peek: " + myQueue.peek())
+    println("pop: " + myQueue.pop())
+    println("queue: " + myQueue.queueStack)
+    println("peek: " + myQueue.peek())
+    println("is queue empty: " + myQueue.empty())
 }
