8653e3cff71eb88e1023195cf5e04dd52792bdf0
Fani Abdullah <faniabdullah27@gmail.com>
2021-08-12 14:54:30 +0700

    understanding dp problem overlapping

--- src/dynamicprogramming/Fibonacci.kt
+++ src/dynamicprogramming/Fibonacci.kt
@@ -2,19 +2,30 @@ package dynamicprogramming
 
 class Fibonacci {
 
-    private val lookupTable: IntArray = IntArray(40){it}
+    private val lookupTable: IntArray = IntArray(40) { -1 }
 
+    fun fibRecursive(n: Int): Int {
+        if (n <= 1) {
+            return n
+        }
+        return fibRecursive(n - 1) + fibRecursive(n - 2)
+    }
+
+    // overlapping sub problem
     fun fib(n: Int): Int {
-        if (lookupTable[n] <= 1) {
-            lookupTable[n] = n
-        } else {
-            lookupTable[n] = fib(n - 1) + fib(n - 2)
+        if (lookupTable[n] == -1) {
+            if (n <= 1) {
+                lookupTable[n] = n
+            } else {
+                lookupTable[n] = fib(n - 1) + fib(n - 2)
+            }
         }
 
         return lookupTable[n]
     }
 }
 
-fun main(){
+fun main() {
+    println(Fibonacci().fibRecursive(6))
     println(Fibonacci().fib(6))
 }
