67e73c4e189a8298615e86d7901a5bbc4c47855a
Fani Abdullah <faniabdullah27@gmail.com>
2021-08-14 11:42:30 +0700

    minimum path sum and reverse word

---
+++ src/compotitiveProgramming/medium/MinimumPathSum.kt
@@ -0,0 +1,37 @@
+package compotitiveProgramming.medium
+
+class MinimumPathSum {
+    fun minPathSum(grid: Array<IntArray>): Int {
+        val m = grid.size
+        val n = grid[0].size
+        val dp = Array(m) { Array(n) { 0 } }
+
+        for (i in 0 until m) {
+            for (j in 0 until n) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j]
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j]
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j]
+                } else {
+                    dp[i][j] = minOf(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
+                }
+            }
+        }
+        return dp[m - 1][n - 1]
+    }
+
+}
+
+fun main() {
+    // [[1,3,1],[1,5,1],[4,2,1]]
+    println(
+        MinimumPathSum().minPathSum(
+            arrayOf(
+                intArrayOf(1, 3, 1), intArrayOf(1, 5, 1),
+                intArrayOf(4, 2, 1)
+            )
+        )
+    )
+}
--- src/dynamicprogramming/LongestIncreasingSubsequence.kt
+++ src/dynamicprogramming/LongestIncreasingSubsequence.kt
@@ -75,16 +75,54 @@ class LongestIncreasingSubsequence {
 
         return max
     }
+
+    private val lookupTable = IntArray(200) { -1 }
+    private val nell = -1
+    fun climbingStairMemoization(n: Int): Int {
+        if (lookupTable[n] == nell) {
+            if (n <= 1) {
+                return 1
+            }
+            lookupTable[n] = climbingStairMemoization(n - 1) + climbingStairMemoization(n - 2)
+        }
+
+        return lookupTable[n]
+    }
+
+    fun climbingStairBottomUp(n: Int): Int {
+        if (n <= 1) {
+            return 1
+        }
+        val dp = IntArray(n + 1)
+        dp[1] = 1
+        dp[2] = 2
+        for (i in 3..n) {
+            dp[i] = dp[i - 1] + dp[i - 2]
+        }
+
+        return dp[n]
+    }
 }
 
 fun main() {
-    val lisOFArray = LongestIncreasingSubsequence()
-        .lis(intArrayOf(0, 1, 0, 3, 2, 3))
+//    val lisOFArray = LongestIncreasingSubsequence()
+//        .lis(intArrayOf(0, 1, 0, 3, 2, 3))
+//    val lisOFArray2 = LongestIncreasingSubsequence()
+//        .longestIncreasingSubSequence(intArrayOf(0, 1, 0, 3, 2, 3))
+    val nameClass = LongestIncreasingSubsequence()
 
+    println(nameClass.climbingStairMemoization(2))
+    println(nameClass.climbingStairMemoization(3))
+    println(nameClass.climbingStairMemoization(4))
+    println(nameClass.climbingStairMemoization(5))
+    println(nameClass.climbingStairMemoization(6))
+    println(nameClass.climbingStairMemoization(7))
+    println(nameClass.climbingStairMemoization(8))
+    println(nameClass.climbingStairMemoization(9))
 
-    val lisOFArray2 = LongestIncreasingSubsequence()
-        .longestIncreasingSubSequence(intArrayOf(0, 1, 0, 3, 2, 3))
+    println(nameClass.climbingStairMemoization(10))
 
-    println(lisOFArray)
-    println(lisOFArray2)
+    println(nameClass.climbingStairMemoization(11))
+    println()
+    println(55 + 89)
 }
