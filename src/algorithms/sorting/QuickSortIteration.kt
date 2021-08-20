e32e868a4011adba8b55fcf4d1af3dec292d5324
Fani Abdullah <faniabdullah27@gmail.com>
2021-08-21 00:43:28 +0700

    quick sort

---
+++ src/algorithms/sorting/QuickSortIteration.kt
@@ -0,0 +1,40 @@
+package algorithms.sorting
+
+class QuickSortIteration {
+
+    fun quickSort(intArray: IntArray, low: Int, high: Int) {
+        if (low < high) {
+            val pi = partition(intArray, low, high)
+            quickSort(intArray, low, pi - 1)
+            quickSort(intArray, pi + 1, high)
+        }
+    }
+
+    private fun partition(intArray: IntArray, low: Int, high: Int): Int {
+        var indexPartition = low - 1
+        val pipot = intArray[high]
+        println("$pipot that's pipot ${intArray.contentToString()}")
+        for (i in low..high) {
+            if (intArray[i] <= pipot) {
+                indexPartition++
+                val temp = intArray[indexPartition]
+                intArray[indexPartition] = intArray[i]
+                intArray[i] = temp
+
+            }
+        }
+
+
+        println(intArray.contentToString())
+        return indexPartition
+    }
+
+
+}
+
+fun main() {
+    val quickSortIteration = QuickSortIteration()
+    val array = intArrayOf(90,100,80 , 70)
+    quickSortIteration.quickSort(array, 0, array.size - 1)
+    println(array.contentToString())
+}
