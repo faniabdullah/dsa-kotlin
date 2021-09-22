afaca88d73cc9063493913314be0de759f38c2cb
Fani Abdullah <faniabdullah27@gmail.com>
2021-09-23 02:14:56 +0700

    fibonacci and tribonacci

---
+++ src/leetcode_badge/data_structure/Day1.kt
@@ -0,0 +1,27 @@
+package leetcode_badge.data_structure
+
+import kotlin.math.sin
+
+class Day1 {
+    fun singleNumber(nums: IntArray): Int {
+       /*
+            https://leetcode.com/problems/single-number/
+
+            TC: O(N)
+            SC: O(1)
+            // XOR of A XOR A = 0, so all duplicates will cancel each other,
+           // leaving the non duplicate
+        */
+        var singleNumber = 0
+        nums.forEach {
+            singleNumber = singleNumber.xor(it)
+        }
+        return singleNumber
+
+    }
+}
+
+fun main(){
+    println(Day1().singleNumber(intArrayOf(2,2,13,5,7,8,8,7,13)))
+    println(Day1().singleNumber(intArrayOf(2,2,13,5,7,8,8,7,13)))
+}
