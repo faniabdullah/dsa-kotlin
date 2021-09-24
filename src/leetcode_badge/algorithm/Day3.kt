deaec2e6666b0134334eb31697460300c1075ae6
Fani Abdullah <faniabdullah27@gmail.com>
2021-09-25 03:56:55 +0700

    Algo day 3

--- src/leetcode_badge/algorithm/Day3.kt
+++ src/leetcode_badge/algorithm/Day3.kt
@@ -15,6 +15,18 @@ class Day3 {
         }
 
     }
+
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        var l = 0
+        var r = numbers.size - 1
+        while (l < r) {
+            val sum = numbers[l] + numbers[r]
+            if (sum > target) r--
+            else if (sum < target) l++
+            else return intArrayOf(l + 1, r + 1)
+        }
+        return intArrayOf(-1, -1)
+    }
 }
 
 fun main() {
